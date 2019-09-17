d311e278b199dce988db641d1cc478b7e8bd708d
Volosojui <alessandro.kalinin@gmail.com>
2019-09-17 20:46:55 +0300

    Update deps

--- gulp/tasks/styles.js
+++ gulp/tasks/styles.js
@@ -11,24 +11,12 @@ const isDev = process.env.NODE_ENV !== 'production'
 const shouldCreateSourcemap = process.argv.indexOf('--sourcemap=true') !== -1
 const config = require('../config').styles[isDev ? 'local' : 'production']
 
-// https://browserl.ist
-const supportedBrowsers = [
-  'last 3 versions',
-  'ie >= 11',
-  'edge >= 12',
-  'firefox >= 28',
-  'chrome >= 21',
-  'safari >= 8',
-  'opera >= 12.1',
-];
-
 export default function styles(){
   return gulp.src(config.sass.src)
     .pipe(plumber())
     .pipe(gulpIf(shouldCreateSourcemap, sourcemaps.init()))
     .pipe(sass().on('error', sass.logError))
     .pipe(autoprefixer({
-      browsers: supportedBrowsers,
       cascade: false
     }))
     .pipe(gulpIf((shouldCreateSourcemap || !isDev), cleanCSS()))
